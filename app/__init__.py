9353d1255732e8378973552523cfdcd7f867e238
wangyangtot <wangyangtot@gmail.com>
2018-09-29 23:41:49 -0700

    movie Project

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://b84a550e7f719b:c51a034d@heroku_91504066c035296/movie"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://b84a550e7f719b:c51a034d@us-cdbr-iron-east-01.cleardb.net/movie"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY']='3c23b72940404fcdb463476d5fb3c0da'
 app.config['UP_DIR']=os.path.join(os.path.abspath(os.path.dirname(__file__)),"static/uploads/")
